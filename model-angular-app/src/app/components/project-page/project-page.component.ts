90d01700e44fa52b1ab740dce688339ab6138a08
Frederick Swadling <frederick.swadling@l-p-a.com>
2019-10-30 22:41:34 +0100

    replace subscribe with switchMap

--- model-angular-app/src/app/components/project-page/project-page.component.ts
+++ model-angular-app/src/app/components/project-page/project-page.component.ts
@@ -4,7 +4,7 @@ import { Observable, combineLatest } from 'rxjs';
 import { RouterLoadingService } from 'src/app/services/router-loading.service';
 import { ProjectUpdateService } from 'src/app/services/project-update.service';
 import { Exposure } from 'src/app/models/exposure';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { Project } from 'src/app/models/project';
 
 @Component({
@@ -12,8 +12,7 @@ import { Project } from 'src/app/models/project';
   templateUrl: './project-page.component.html',
   styleUrls: ['./project-page.component.scss']
 })
-export class ProjectPageComponent implements OnInit {
-
+export class ProjectPageComponent {
   constructor(
     private actr: ActivatedRoute,
     private routerLoadingService: RouterLoadingService,
@@ -21,18 +20,19 @@ export class ProjectPageComponent implements OnInit {
 
   isRouting$ = this.routerLoadingService.isRouting$;
 
-  exposures$: Observable<Exposure[]> = this.projectUpdateService.exposures$;
+  private exposures$: Observable<Exposure[]> = this.projectUpdateService.exposures$;
 
-  project$: Observable<Project> = this.actr.data.pipe(map(data => data.project as Project));
+  private project$: Observable<Project> = this.actr.data.pipe(map(data => data.project as Project));
 
-  data$ = combineLatest([
+  private combined$ = combineLatest([
     this.project$,
     this.exposures$
   ]);
 
-  ngOnInit() {
-    this.actr.data.subscribe(data => {
+  data$ = this.actr.data.pipe(
+    switchMap(data => {
       this.projectUpdateService.update(data.exposures);
-    });
-  }
+      return this.combined$;
+    })
+  );
 }
